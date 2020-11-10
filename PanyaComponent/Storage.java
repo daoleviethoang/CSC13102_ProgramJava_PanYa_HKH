66b42ad1593fb87ecb68d51fff0104febdba598f
qhuydo <q.huy.do.172@gmail.com>
2020-11-10 14:35:03 +0700

    Code implementation from UML

--- PanyaComponent/Storage.java
+++ PanyaComponent/Storage.java
@@ -1,32 +1,37 @@
-package Library;
-import java.util.*;
-public class Storage{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
     int capacity;
     List<Ingredient> list = new ArrayList<Ingredient>();
-    Storage()
-    {
-        capacity = 0;
+
+    Storage() {
+        this.capacity = 0;
     }
-    Storage(int cap, List<Ingredient> l)
-    {
+
+    Storage(int cap, List<Ingredient> l) {
         this.capacity = cap;
-        this.list = l;
+        this.list = new ArrayList<>(l);
     }
-    Storage(Storage s)
-    {
+
+    Storage(Storage s) {
         this.capacity = s.getCapacity();
-        this.list = s.getList();
+        this.list = new ArrayList<>(s.getList());
     }
+
     public int getCapacity() {
         return capacity;
     }
+
     public List<Ingredient> getList() {
         return list;
     }
+
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
+
     public void setList(List<Ingredient> list) {
-        this.list = list;
+        this.list = new ArrayList<>(list);
     }
 }

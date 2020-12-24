28fd5a838e1e0850f1e6bd5062103ea68d4a1b67
Hoàng Xuân Kiệt <48423316+kiethoang214@users.noreply.github.com>
2020-12-24 17:08:43 +0700

    Add storage window
    [+] StorageWindow.java thêm StorageWindow panel
    [*] ApplicationWindow sửa để thêm StorageWindow

---
+++ Chameleon.java
@@ -0,0 +1,7 @@
+package PanyaUI;
+import java.awt.Color;
+public interface Chameleon {
+    public void initTheme(String themeName);
+    public void initTheme(Color primary, Color light, Color dark);
+
+}

74bb871bf1535e5172b68530ee72afb24bc9e249
Hoàng Xuân Kiệt <kiethoang611@gmail.com>
2021-01-04 17:24:20 +0700

    add + delete + importIngredient + search methods

--- Panya/src/main/java/PanyaCore/Storage.java
+++ Panya/src/main/java/PanyaCore/Storage.java
@@ -2,7 +2,7 @@ package PanyaCore;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.math.BigDecimal;
 /**
  * Object Storage quản lý nhà kho trong phần mềm Pan-ya
  */
@@ -51,5 +51,45 @@ public class Storage {
     public void setList(List<Ingredient> list) {
         this.list = new ArrayList<>(list);
     }
+    public void add(Ingredient i) {
+        list.add(i);
+    }
+    public void delete(String id) {
+        for(int i = list.size() - 1; i >= 0 ;i++) {
+            if (list.get(i).getId().compareTo(id) == 0) {
+                list.remove(i);
+            }
+        }
+    }
+    public void update(Ingredient in) {
+        for(int i = list.size() - 1; i >= 0 ;i++) {
+            if (list.get(i).getId().compareTo(in.getId()) == 0) {
+                list.get(i).setName(in.getName());
+                list.get(i).setQuantity(in.getQuantity());
+                list.get(i).setUnit(in.getUnit());
+                list.get(i).setPrice(in.getPrice());
+                list.get(i).setNote(in.getNote());
 
+            }
+        }
+    }
+    public void importIngre(String id, BigDecimal q) {
+        for(int i = list.size() - 1; i >= 0 ;i++) {
+            if (list.get(i).getId().compareTo(id) == 0) {
+                list.get(i).setQuantity(q);
+            }
+        }
+    }
+    public List<Ingredient> search(String text) {
+        List<Ingredient> result = new ArrayList<Ingredient>();
+        String[] part_text = text.split(" ");               //lấy từng từ trong text
+        for(int i = 0; i < list.size(); i++) {              //xét từng ingredient
+            for(int j = 0; j < part_text.length; j++){      //xem có từ nào trong name hay ko?
+                if(list.get(i).getName().contains(part_text[j])){
+                    result.add(list.get(i));
+                }
+            }
+        }
+        return result;
+    }
 }
